a7f06bf6a75043695a83588ba5dd347b5b02271d
e-h-dev <ebaal8226@gmail.com>
2025-12-17 00:51:09 +0000

    Create models for tutors database

--- tutors/models.py
+++ tutors/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Model to contain all possible registered
+Locations from which tutors operate from.
+"""
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+"""
+Model to contain the various categories
+of subjects a user can find a tutor from.
+"""
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+"""
+Model to store all tutor information
+"""
+
+
+class Tutors(models.Model):
+    name = models.CharField(max_length=254)
+    location = models.ForeignKey('Location', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    date_added = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+    description = models.TextField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=14)
+    rating = models.IntegerField()
+    reviews = models.TextField()
+    image = models.ImageField()
+    is_male = models.BooleanField()
+
+    def __str__(self):
+        return self.name
