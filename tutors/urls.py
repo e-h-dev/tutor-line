6788cf721495795115498877871b4a10e9d0d3a2
e-h-dev <ebaal8226@gmail.com>
2025-12-18 12:51:21 +0000

    Set up tutor details page, view and url

--- tutors/urls.py
+++ tutors/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tutors, name='tutors')
+    path('', views.tutors, name='tutors'),
+    path('<int:tutor_id>', views.tutor_details, name='tutor_details')
 ]
--- tutors/views.py
+++ tutors/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Location, Category, Tutors
 
 
@@ -12,3 +12,16 @@ def tutors(request):
     context = {'tutors': tutors}
     return render(request, 'tutors/tutors.html', context)
 
+
+def tutor_details(request, tutor_id):
+    """
+    A view to show the details of each individual tutor
+    """
+
+    tutor = get_object_or_404(Tutors, pk=tutor_id)
+
+    template = 'tutors/tutor-details.html'
+
+    context = {'tutor':tutor}
+
+    return render(request, template, context)
