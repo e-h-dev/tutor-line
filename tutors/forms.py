ac640d3502150c674729cac7f6f753d1411b302e
e-h-dev <ebaal8226@gmail.com>
2025-12-25 16:44:43 +0000

    Update and style form appearance

--- tutors/forms.py
+++ tutors/forms.py
@@ -7,10 +7,15 @@ class TutorForm(forms.ModelForm):
     class Meta:
         model = Tutors
         fields = '__all__'
+        widgets = {
+            'is_male': forms.Select(choices=[
+                (None, 'Gender...'),
+                (True, 'Male'),
+                (False, 'Female'),
+            ])
+        }
 
     image = forms.ImageField(label='Image', required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    
