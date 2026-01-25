c9707824be8d75358d4e2cbcfea79ac7c1701711
e-h-dev <ebaal8226@gmail.com>
2026-01-25 21:39:58 +0000

    Updating view to show acurate messgae count

--- messaging/views.py
+++ messaging/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 from tutors.models import Tutors
 from .models import Message
 from .forms import MessageForm
@@ -10,8 +11,17 @@ from .forms import MessageForm
 def messaging(request):
 
     message = Message.objects.all()
+    
+    my_messages = Message.objects.filter(send_to=User.username)
 
-    return render(request, 'messaging/messaging.html', {'message': message})
+    message_count = len(my_messages)
+
+    context = {
+        'message': message,
+        'message_count': message_count,
+    }
+
+    return render(request, 'messaging/messaging.html', context)
 
 
 def compose_message(request, tutor_id):
@@ -22,7 +32,8 @@ def compose_message(request, tutor_id):
         form = MessageForm(request.POST, tutor_id)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Your message has been sent to {compose.name}')
+            messages.success(request, f'Your message has been\
+                             sent to {compose.name}')
             return redirect('tutors')
     
     else:
