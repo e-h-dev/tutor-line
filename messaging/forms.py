149148a95f8b4ec2fbe95465bbd8562bb28fdb06
e-h-dev <ebaal8226@gmail.com>
2026-01-25 11:43:20 +0000

    Finalise messaging function to only send message to send_to profile

--- messaging/forms.py
+++ messaging/forms.py
@@ -7,7 +7,15 @@ class MessageForm(forms.ModelForm):
 
     class Meta:
         model = Message
-        fields = ['name', 'Phone', 'email', 'message']
+        fields = '__all__'
+        # fields = ['send_to', 'name', 'Phone', 'email', 'message']
+        # widgets = {
+        #     'send_to': forms.TextInput(attrs={'value': 'RebbeGreen'}),
+        #     'name': forms.TextInput(attrs={'placeholder': "Your Name"}),
+        #     'Phone': forms.TextInput(attrs={'placeholder': "Your Phone Number"}),
+        #     'email': forms.EmailInput(attrs={'placeholder': "Your Email"}),
+        #     'message': forms.Textarea(attrs={'placeholder': "Your Message"}),
+        # }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
