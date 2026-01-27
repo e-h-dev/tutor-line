e2998901775b411f65eff2f7b53c72c35eb16380
e-h-dev <ebaal8226@gmail.com>
2026-01-27 20:18:42 +0000

    Update message notification styles

--- messaging/contexts.py
+++ messaging/contexts.py
@@ -9,11 +9,16 @@ def messaging(request):
     
     my_messages = Message.objects.filter(send_to=current_user)
 
+    unread_messages = my_messages.filter(read=False)
+
     message_count = len(my_messages)
+
+    unread_count = len(unread_messages)
 
     context = {
         'message': message,
         'message_count': message_count,
+        'unread_count': unread_count,
     }
 
     return context
